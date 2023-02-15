2bcd083cc14fd7efb73ce2895df60565dc6a8067
Luis Mejia <luisrvp5@gmail.com>
2023-02-15 13:06:55 -0600

    creacion de objeto y uso de la clase
    Creamos los objetos que serian todos los miembros que van a la iglesia y usamos la clase que creamos en el commit anterior

--- src/index.ts
+++ src/index.ts
@@ -64,3 +64,20 @@ interface IChurchMember {
      }
 
 }
+
+
+// creamos las instancias con usaremos la clase attendancestracker
+const registrador = new RegistroAsistencia()
+
+
+// hacemos objetos donde agregamos a nuestros miembros 
+registrador.addMember({ id: 1, nombre: 'John Doe', email: 'john.doe@gmail.com', telefono: '+504 99776354', memberActivo: true })
+registrador.addMember({ id: 2, nombre: 'Jane Smith', email: 'jane.smith@egmail.com', telefono: '+504 34776354', memberActivo: true })
+registrador.addMember({ id: 3, nombre: 'Erwin Smith', email: 'erwinsmith@attack.com', telefono: '+504 99476346', memberActivo: true })
+registrador.addMember({ id: 4, nombre: 'Capitan Levi', email: 'Capitan@gmail.com', telefono: '+504 99953217', memberActivo: true })
+registrador.addMember({ id: 5, nombre: 'Eren Jeager', email: 'titandeataque@gmail.com', telefono: '+1 202 9833175', memberActivo: true })
+registrador.addMember({ id: 6, nombre: 'Thor Odinson', email: 'Thor@gmail.com', telefono: '+504 98532698', memberActivo: true })
+registrador.addMember({ id: 7, nombre: 'Helton Lopez', email: 'Helton@gmail.com', telefono: '+504 3387491', memberActivo: true })
+registrador.addMember({ id: 8, nombre: 'Ivanna Konstantinovna Perepeshko', email: 'Ivanna@gmail.com', telefono: '+9 6584765341', memberActivo: true })
+registrador.addMember({ id: 9, nombre: 'Jose David Mejia Castro', email: 'Jose@gmail.com', telefono: '+43 45789324', memberActivo: true })
+registrador.addMember({ id: 10, nombre: 'Luis Fernando Mejia Castro', email: 'Luisrvp5@gmail.com', telefono: '+504 33614633' , memberActivo: true})
